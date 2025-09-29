c15c139cb719d5c0f9b81cd1a0bd2bbd4e5a2318
Yoav Klein <yoavklein25@gmail.com>
2025-09-29 11:31:26 +0000

    k6 operator

---
+++ 06-k8s-operator/README.md
@@ -0,0 +1,14 @@
+# K6 Operator
+---
+
+You can run your K6 tests in a Kubernetes cluster using the K6 operator.
+
+
+## Installation
+---
+
+```
+curl https://raw.githubusercontent.com/grafana/k6-operator/main/bundle.yaml | kubectl apply -f -
+```
+
+
---
+++ 06-k8s-operator/script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 20,
+    duration: '30s',
+    thresholds: {
+        checks: ['rate>0.93'],
+        http_req_duration: ['p(99)<500', 'p(90)<400']
+    }
+};
+
+export default function() {
+    let res = http.get('http://k6-http-server/api/delay/random');
+    check(res, {
+        'check status is 200': (res) => res.status === 200
+    });
+
+}
